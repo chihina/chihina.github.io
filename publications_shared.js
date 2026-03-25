e19c608e05040253deb958b5a33fbe277a984b48
chihiro nakatani <program.student.tti@gmail.com>
2026-03-26 08:48:36 +0900

    update publications

---
+++ publications_shared.js
@@ -0,0 +1,44 @@
+window.sharedPublicationsHtml = `
+<div class="pub-item fade-up">
+  <span class="pub-venue">CVPR 2026 Workshop on Eye and Gaze in Computer Vision (GAZE 2026)</span>
+  <div class="pub-title">End-to-End Shared Attention Estimation via Group Detection with Feedback Refinement</div>
+  <div class="pub-authors"><strong>Chihiro Nakatani</strong>, Norimichi Ukita, Jean-Marc Odobez</div>
+  <div class="pub-collab-note">In collaboration with Prof. Jean-Marc Odobez (Idiap Research Institute / EPFL)</div>
+  <div class="pub-note">Proc. of IEEE Conference on Computer Vision &amp; Pattern Recognition, June 2026.</div>
+</div>
+
+<div class="pub-item fade-up">
+  <span class="pub-venue">CVPR 2026 Findings</span>
+  <div class="pub-title">Group-DINOmics: Incorporating People Dynamics into DINO for Self-supervised Group Activity Feature Learning</div>
+  <div class="pub-authors">Ryuki Tezuka, <strong>Chihiro Nakatani</strong>, Norimichi Ukita</div>
+  <div class="pub-note">Proc. of IEEE Conference on Computer Vision &amp; Pattern Recognition, June 2026.</div>
+</div>
+
+<div class="pub-item fade-up">
+  <span class="pub-venue journal">CVIU 2026</span>
+  <div class="pub-title"><a href="https://www.toyota-ti.ac.jp/Lab/Denshi/iim/ukita/selection/CVIU2026-GAFL.html">Human-in-the-loop Adaptation in Group Activity Feature Learning for Team Sports Video Retrieval</a></div>
+  <div class="pub-authors"><strong>Chihiro Nakatani</strong>, Hiroaki Kawashima, Norimichi Ukita</div>
+  <div class="pub-note">Computer Vision and Image Understanding, vol.263, pp. 104577, 2026. Special issue on "Computer Vision for Sports and Winter Sports".</div>
+</div>
+
+<div class="pub-item fade-up">
+  <span class="pub-venue">ICCV 2025</span>
+  <div class="pub-title"><a href="https://www.toyota-ti.ac.jp/Lab/Denshi/iim/ukita/selection/ICCV2025-new.html">Dynamic Group Detection using VLM-augmented Temporal Groupness Graph</a></div>
+  <div class="pub-authors">Kaname Yokoyama, <strong>Chihiro Nakatani</strong>, Norimichi Ukita</div>
+  <div class="pub-note">Proc. of IEEE International Conference on Computer Vision, Oct. 2025. (acceptance rate = 24.0%)</div>
+</div>
+
+<div class="pub-item fade-up">
+  <span class="pub-venue">CVPR 2024</span>
+  <div class="pub-title"><a href="https://www.toyota-ti.ac.jp/Lab/Denshi/iim/ukita/selection/CVPR2024-GAFL.html">Learning Group Activity Features Through Person Attribute Prediction</a></div>
+  <div class="pub-authors"><strong>Chihiro Nakatani</strong>, Hiroaki Kawashima, Norimichi Ukita</div>
+  <div class="pub-note">Proc. of IEEE Conference on Computer Vision &amp; Pattern Recognition, June 2024. (acceptance rate = 23.6%)</div>
+</div>
+
+<div class="pub-item fade-up">
+  <span class="pub-venue">ICCV 2023</span>
+  <div class="pub-title"><a href="https://www.toyota-ti.ac.jp/Lab/Denshi/iim/ukita/selection/ICCV2023-PJAE.html">Interaction-aware Joint Attention Estimation Using People Attributes</a></div>
+  <div class="pub-authors"><strong>Chihiro Nakatani</strong>, Hiroaki Kawashima, Norimichi Ukita</div>
+  <div class="pub-note">Proc. of IEEE International Conference on Computer Vision, Oct. 2023. (acceptance rate = 26.15%)</div>
+</div>
+`;
